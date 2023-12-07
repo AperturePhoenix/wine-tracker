95ea848ab39e575daf2291b60529f4665937b5e8
Lance Judan <aperture.phoenix@gmail.com>
2023-12-07 01:36:29 +0000

    add linter to frontend

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,17 +2,29 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  plugins: ['react-refresh', 'prettier'],
   rules: {
+    'import/no-absolute-path': 'off',
+    "import/no-extraneous-dependencies": ["error", {"devDependencies": ["vite.config.ts", "**/*.spec.js"]}],
+    'prettier/prettier': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    
   },
 }
