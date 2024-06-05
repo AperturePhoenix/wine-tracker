6fb92b558d56440c3f1bd18d9afaf446e9dde916
Lance Judan <aperture.phoenix@gmail.com>
2024-06-05 02:16:40 +0000

    basic login page

--- packages/frontend/src/routes/__root.tsx
+++ packages/frontend/src/routes/__root.tsx
@@ -1,12 +1,21 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router"
-import { AppBar, Toolbar } from "@mui/material"
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router"
+import { AppBar, Button, Toolbar, Typography } from "@mui/material"
 
 export const Route = createRootRoute({
   component: () => (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <AppBar position="static">
-        <Toolbar>This is the app toolbar</Toolbar>
+    <div style={{ width: "100vw", maxWidth: "100vw", height: "100vh", maxHeight: "100vh" }}>
+      <AppBar>
+        <Toolbar>
+          {/* TODO: Add app icon here */}
+          <Typography variant="h5" flexGrow={1}>
+            Wine Tracker
+          </Typography>
+          <Link to="/login">
+            <Button style={{ color: "white" }}>Login</Button>
+          </Link>
+        </Toolbar>
       </AppBar>
+      <Toolbar />
       <Outlet />
     </div>
   ),
--- packages/frontend/src/routes/login.lazy.tsx
+++ packages/frontend/src/routes/login.lazy.tsx
@@ -1,3 +1,4 @@
+import { Button, Card, CardContent, Stack, TextField, Typography } from "@mui/material"
 import { createLazyFileRoute } from "@tanstack/react-router"
 
 export const Route: unknown = createLazyFileRoute("/login")({
@@ -5,5 +6,22 @@ export const Route: unknown = createLazyFileRoute("/login")({
 })
 
 function Login() {
-  return <>This is the login page</>
+  return (
+    <Stack
+      direction="row"
+      margin={5}
+      style={{ justifyContent: "center", alignItems: "center", alignContent: "center" }}
+    >
+      <Card>
+        <CardContent>
+          <Stack direction="column" spacing={2}>
+            <Typography variant="h5">Login</Typography>
+            <TextField label="Username" />
+            <TextField label="Password" type="password" />
+            <Button variant="contained">Submit</Button>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Stack>
+  )
 }
