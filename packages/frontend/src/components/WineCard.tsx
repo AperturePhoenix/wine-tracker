062b9cfd1141d20dab66f904afc44c595a9193ee
Lance Judan <aperture.phoenix@gmail.com>
2024-06-13 20:04:12 +0000

    add basic card for wines

---
+++ packages/frontend/src/components/WineCard.tsx
@@ -0,0 +1,27 @@
+import { Button, Card, CardActions, CardContent, Stack, Typography } from "@mui/material"
+import type { Wine } from "wine-tracker-models"
+import { useUser } from "../hooks"
+
+export interface WineCardProps {
+  wine: Wine
+}
+
+export default function WineCard({ wine }: WineCardProps): JSX.Element {
+  const user = useUser()
+  return (
+    <Card sx={{ width: "fit-content" }}>
+      <CardContent>
+        <Stack direction="row" spacing={2}>
+          <Typography variant="h5">{wine.name}</Typography>
+          <Typography variant="h5" color="text.secondary">
+            {wine.brand}
+          </Typography>
+        </Stack>
+      </CardContent>
+      <CardActions>
+        <Button>Show Reviews</Button>
+        {user && <Button>Add a Review</Button>}
+      </CardActions>
+    </Card>
+  )
+}
--- packages/frontend/src/routes/index.lazy.tsx
+++ packages/frontend/src/routes/index.lazy.tsx
@@ -3,6 +3,7 @@ import { Link, createLazyFileRoute } from "@tanstack/react-router"
 import { useEffect, useState } from "react"
 import type { Wine } from "wine-tracker-models"
 import { getWines } from "../api"
+import WineCard from "../components/WineCard"
 
 export const Route: unknown = createLazyFileRoute("/")({
   component: Index,
@@ -18,7 +19,7 @@ function Index() {
   return (
     <Stack direction="column" spacing={2}>
       {wines?.map((wine) => (
-        <Stack key={wine.id}>{wine.name}</Stack>
+        <WineCard key={wine.id} wine={wine} />
       ))}
       <div className="p-2">
         <Link to="/create-wine">
